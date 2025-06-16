2a291ef29ad754a04f20377fe3c766e337f68396
Documenter.jl <documenter@juliadocs.github.io>
2025-06-16 17:43:41 +0000

    build based on 8bc6a1d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FewBodyHamiltonians","page":"Home","title":"FewBodyHamiltonians","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FewBodyHamiltonians.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
